c9aa545ef6bd01e8e342cc9d9f53cf925e822071
Shashank Dubey <jfriday464@gmail.com>
2022-01-19 16:01:56 +0530

    Testing bit.dev components

---
+++ react-test-app/src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import { Heading } from "@jfriday464/learningbit.ui.heading";
+
+function Home() {
+   return (
+      <div>
+         <h1>This is Home page</h1>
+         <Heading element="h3" className="code">
+            This is heading
+         </Heading>
+      </div>
+   );
+}
+
+export default Home;
